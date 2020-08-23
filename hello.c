772629fd2c38e28a8f5842c9e47e3dcee290f0f2
Vasavi Kotipalli <vkotipalli2@huskers.unl.edu>
2020-08-23 00:38:14 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Vasavi Kotipalli
+ * Date: Aug 18 2020
+ *
+ * a simple hello world program
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Vasavi Kotpalli!\n");
+    printf("Computer Science\n");
+
+    return 0;
+
+}
